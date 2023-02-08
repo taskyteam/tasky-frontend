2c559d5a24c68908b4173dc5d954a04689c9ee93
audunlk <audunlk@gmail.com>
2023-02-08 10:45:21 +0100

    funker det nå

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,15 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            currentUser: {},
+            tasks: [],
+            users: [],
         }
     }
 render() {
     return(
 <div>
-    <h1>Welcome</h1>
+    <h1>Welcome to you</h1>
 </div>
     );
 }
