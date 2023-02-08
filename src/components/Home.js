b70077f25c5976c2968e903947b50bee619ce9f1
audunlk <audunlk@gmail.com>
2023-02-08 10:04:05 +0100

    added createTask

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import { Component } from "react";
 
 
-class Home extends Comment {
+class Home extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            
         }
     }
 render() {
