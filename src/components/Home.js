f2558193e9cf67d572150c95fe3a97f0d86b5911
audunlk <audunlk@gmail.com>
2023-02-08 10:38:55 +0100

    added routing

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,8 @@ import { Component } from "react";
 class Home extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            
+
         }
     }
 render() {
