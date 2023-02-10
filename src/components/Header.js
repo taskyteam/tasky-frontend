b5606d28dfdd10aaf3167ada88d0a919e2719e2d
VictoriaOverbo <122522571+VictoriaOverbo@users.noreply.github.com>
2023-02-10 12:57:27 +0100

    Moved the burger to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,48 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 
 class Header extends React.Component{
     render(){
         return(
             <div>
-                <h1>Tasky</h1>
-            </div>
+                 {[false].map((expand) => (
+        <Navbar key={expand} expand={expand}>
+          <h1>Logo</h1>
+            <Navbar.Toggle/>
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}></Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav>
+                  <Nav.Link 
+                  to="/Home"
+                  onClick={() => {
+                    window.location.href = "/Home"; }}>
+                        Home</Nav.Link>
+                  <Nav.Link 
+                  to="/Faq"
+                  onClick={() => {
+                    window.location.href = "/Faq"; }}>
+                        FAQ</Nav.Link>
+                  <Nav.Link 
+                  to="/Contact"
+                  onClick={() => {
+                    window.location.href = "/Contact"; }}>
+                        Contact</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+        </Navbar>
+      ))}
+      </div>
+          
 
         )
     }
